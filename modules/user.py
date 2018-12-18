455b96adb1a2cdd11d1358d4eeb6ee1694c4c63c
joyc <joyc@live.jp>
2018-12-18 23:56:43 +0900

    add register user class and check user functions

--- modules/user.py
+++ modules/user.py
@@ -8,6 +8,23 @@ class User:
         self.email = email
         self.password = password
 
+    @staticmethod
+    def register_user(name, email, password):
+        user_data = Database.find_one(collection="users", query={"email": email})
+        if user_data is not None:
+            return False
+        User(name, email, User.hash_password(password)).save_to_db()
+        return True
+
+    @staticmethod
+    def check_user(email, password):
+        user_data = Database.find_one(collection="users", query={"email": email})
+        if user_data is None:
+            return False
+        if User.check_password(password, user_data['password']) is False:
+            return False
+        return True
+
     def save_to_db(self):
         Database.insert_many(collection="users", data=self.json())
 
@@ -19,9 +36,13 @@ class User:
         }
 
     @staticmethod
-    def hash_password(self, password):
+    def hash_password(password):
         return pbkdf2_sha512.hash(password)
 
     @staticmethod
-    def check_password(self, password, hash_password):
+    def check_password(password, hash_password):
         return pbkdf2_sha512.verify(password, hash_password)
+
+    @staticmethod
+    def find_user_data(email):
+        return Database.find_one(collection="users", query={"email": email})
