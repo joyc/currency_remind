66d798db98d97e71f63b2c6020cc696c858c2df2
joyc <joyc@live.jp>
2018-12-20 01:30:17 +0900

    put data on home.html

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from modules.money import Money
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    monen_dict, positon = Money.search_data()
+    return render_template('home.html', money_dict=monen_dict, positon=positon)
 
 
 if __name__ == '__main__':
